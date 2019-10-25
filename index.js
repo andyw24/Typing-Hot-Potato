7ec6b10847193054ae9a6d22396f1bf570574eb1
andyw24 <andyw24@gmail.com>
2019-10-25 10:03:41 -0400

    user can 'login' but session is not saved
    this is a commit prior to installing express-socket.io-session

--- index.js
+++ index.js
@@ -9,23 +9,38 @@ var users = [];
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/pages/index.html');
+  res.sendFile(__dirname + '/public/pages/login.html');
 });
 
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  users.push("test");
+
+  socket.on('login', function(usr){
+    socket['username'] = usr;
+    users.push(socket.id);
+    console.log("A NEW USER HAS LOGGED IN: " + socket['username']);
+    console.log("userlist: " + users);
+    
+    //redirect them to roomJoining.html
+    var destination = '/public/pages/roomJoining.html';
+    socket.emit('redirect', destination);
+  })
+
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+    for( var i = 0; i < users.length; i++){ 
+      if ( users[i] === socket.id) {
+        users.splice(i, 1); 
+      }
+    }
+    console.log("disconnected userlist: " + users);
+  });
 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
     console.log("userlist: " + users);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    users.pop();
   });
 });
 
