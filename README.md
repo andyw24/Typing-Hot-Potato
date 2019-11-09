636242e6edd709ec9884c9d26a295abbb1cbabe0
andyw24 <andyw24@gmail.com>
2019-11-08 21:29:54 -0500

    npm run dev
    vaguely followed this guide: https://www.freecodecamp.org/news/how-to-deploy-a-react-app-with-an-express-server-on-heroku-32244fe5a250/
    
    - mainly npm concurrently
    - npm run dev will start node and react apps at the same time

--- README.md
+++
@@ -1,2 +0,0 @@
-# Typing Hot Potato
- An online multiplayer typing game based on the classic Hot Potato game. 
--- server.js
+++ server.js
@@ -1,110 +1,32 @@
-const express = require("express");
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-const port = process.env.PORT || "5001";
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+//Route setup
+app.get('/', (req, res) => {
+	res.send('root route');
+})
 
-//----------------session----------------
-var session = require("express-session")({
-    secret: "session.hidden",
-    username: "default0",
-    resave: true,
-    saveUninitialized: true
-});
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-var sharedsession = require("express-socket.io-session");
-// Use express-session middleware for express
-app.use(session);
-// Use shared session middleware for socket.io
-// setting autoSave:true
-io.use(sharedsession(session, {
-    autoSave:true
-})); 
-//----------------session----------------
+//production mode
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));  
+	//
+	app.get('*', (req, res) => {
+		res.sendfile(path.join(__dirname = 'client/build/index.html'));
+	})
+}
 
+//build mode
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
-var users = [];
-var allRooms = [];
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('login', function(usr){
-    //socket['username'] = usr;
-    socket.handshake.session.username = usr;
-    users.push(socket.id);
-    console.log("A NEW USER HAS LOGGED IN: " + socket.handshake.session.username);
-    console.log("userlist: " + users);
-    
-    //redirect them to roomJoining.html
-    var destination = '/public/pages/roomJoining.html';
-    socket.emit('redirect', destination);
-  })
-
-  socket.on('createRoom', function(room) {
-    console.log(socket.handshake.session.username + " IS ATTEMPTING TO CREATE A NEW ROOM")
-    var successfulCreateRoom = false;
-    var roomCode;
-    var index = 0;
-    while (!successfulCreateRoom) {
-      successfulCreateRoom = true;
-      roomCode = Math.floor(100000 + Math.random() * 900000);
-      for (index = 0; index < allRooms.length; index++) {
-        console.log(allRooms[index]);
-        if (allRooms[index] == roomCode) {
-          successfulCreateRoom = false;
-        }
-      }
-    }
-
-    socket.join(roomCode);
-    allRooms.push(roomCode);
-    console.log("ADDED: " + allRooms[index]);
-
-    console.log(io.sockets.adapter.rooms[allRooms[index]]);
-  })
-
-  socket.on('joinRoom', function(code) {
-    console.log(socket.handshake.session.username + " IS ATTEMPTING TO JOIN ROOM: " + code);
-    var successfulJoinRoom = false;
-    for (index = 0; index < allRooms.length; index++) {
-      console.log(allRooms[index]);
-      if (allRooms[index] == code) {
-        socket.join(allRooms[index]);
-        console.log(socket.handshake.session.username + " JOINED ROOM: " + code);
-        console.log(io.sockets.adapter.rooms[allRooms[index]]);
-        successfulJoinRoom = true;
-      }
-    }
-    if (!successfulJoinRoom) {
-      console.log(socket.handshake.session.username + " FAILED TO JOIN ROOM: " + code);
-    } 
-  })
-
-  socket.on('disconnect', function(){
-    console.log('a user disconnected');
-    for( var i = 0; i < users.length; i++){ 
-      if ( users[i] === socket.id) {
-        users.splice(i, 1); 
-      }
-    }
-    console.log("disconnected userlist: " + users);
-  });
-
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-    console.log("userlist: " + users);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on port ' + port);
+//Start server
+app.listen(port, (req, res) => {
+	console.log('server listening on port: ' + port);
+	console.log('hello you fucking g');
 });
